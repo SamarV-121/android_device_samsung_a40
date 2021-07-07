848692db58ca8ae62ff45e43028dc882fe8f6c09
SamarV-121 <samarvispute121@gmail.com>
2021-07-07 13:47:27 +0530

    a40: Decommonize some init scripts
    Change-Id: I4ed6aa575ac6f873da48e064076e611c3a637a41
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -5,6 +5,8 @@ $(call inherit-product, device/samsung/universal7904-common/universal7904-common
 
 $(call inherit-product, vendor/samsung/a40/a40-vendor.mk)
 
-# fstab
+# Rootdir
 PRODUCT_PACKAGES += \
-	fstab.exynos7904
+	fstab.exynos7904 \
+	init.target.rc \
+	init.baseband.rc
