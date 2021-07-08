2268d54b24d7a13b059e6aba4760c4c105b10127
SamarV-121 <samarvispute121@gmail.com>
2021-07-08 19:49:44 +0530

    a40: Import device specific vendor blobs
    Change-Id: I8e7d733ff09a065a4f18070aecd435014b2a137a
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -2,3 +2,5 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Inherit common device configuration
 $(call inherit-product, device/samsung/universal7904-common/universal7904-common.mk)
+
+$(call inherit-product, vendor/samsung/a40/a40-vendor.mk)
