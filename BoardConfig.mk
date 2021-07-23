5cf3f3c13efa0c7b17ad5cb156e9d08f609d5155
SamarV-121 <samarvispute121@pm.me>
2021-07-23 10:57:03 +0530

    a40: Include teegris sepolicy
    Change-Id: Ibe4087a69cae49bf69a26c6c16a6a2c6b550a1f0
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,12 +9,15 @@ TARGET_KEYMASTER_VARIANT := samsung
 # Kernel
 TARGET_KERNEL_CONFIG := a40_defconfig
 
-# Inherit common board flags
-include device/samsung/universal7904-common/BoardConfigCommon.mk
-
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 55574528
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 5158993920
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/recovery.fstab
+
+# Sepolicy
+BOARD_SEPOLICY_TEE_FLAVOR := teegris
+
+# Inherit common board flags
+include device/samsung/universal7904-common/BoardConfigCommon.mk
